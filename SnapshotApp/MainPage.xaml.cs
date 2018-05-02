2d28b4865bbd221e6315f7016c198047cf3e6bf5
Bhairav <bhairavpardiwala@gmail.com>
2018-05-02 21:47:42 +0530

    First Commit

---
+++ SnapshotApp/MainPage.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SnapshotApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture _mediaCapture;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Init();
+        }
+        private async Task Init()
+        {
+            try
+            {
+                // ele = new CaptureElement();
+
+                 _mediaCapture = new MediaCapture();
+                await _mediaCapture.InitializeAsync();
+                previewElement.Source = _mediaCapture;
+                
+                await _mediaCapture.StartPreviewAsync();
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                //await LaunchAppAsync("http:\\www.google.com");
+                 await CapturePhoto();
+                await _mediaCapture.StopPreviewAsync();
+                Application.Current.Exit();
+            }
+            catch (Exception e)
+            {
+
+                Application.Current.Exit();
+            }
+        }
+        public IAsyncAction abc()
+        {
+            return Task.Run(Init).AsAsyncAction();
+        }
+
+        private async Task<String> CapturePhoto()
+        {
+            var myPictures = await Windows.Storage.StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            StorageFile file = await myPictures.SaveFolder.CreateFileAsync(DateTime.Now.ToString("Pdd MM yyyy hh mm ss tt")+".jpg", CreationCollisionOption.GenerateUniqueName);
+
+
+            using (var captureStream = new InMemoryRandomAccessStream())
+            {
+
+                await _mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), captureStream);
+
+                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(captureStream);
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(fileStream, decoder);
+
+                    var properties = new BitmapPropertySet {
+            { "System.Photo.Orientation", new BitmapTypedValue(PhotoOrientation.Normal, PropertyType.UInt16) }
+        };
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+
+                    await encoder.FlushAsync();
+
+
+
+                }
+
+            }
+
+
+
+
+
+            return "";
+        }
+        private async Task LaunchAppAsync(string uriStr)
+        {
+            Uri uri = new Uri(uriStr);
+            var promptOptions = new Windows.System.LauncherOptions();
+            promptOptions.TreatAsUntrusted = false;
+
+            bool isSuccess = await Windows.System.Launcher.LaunchUriAsync(uri, promptOptions);
+
+            if (!isSuccess)
+            {
+                string msg = "Launch failed";
+                // await new MessageDialog(msg).ShowAsync();
+            }
+        }
+
+        private async Task<string> StorageFileToBase64(StorageFile file)
+        {
+            string Base64String = "";
+
+            if (file != null)
+            {
+                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+                var reader = new DataReader(fileStream.GetInputStreamAt(0));
+                await reader.LoadAsync((uint)fileStream.Size);
+                byte[] byteArray = new byte[fileStream.Size];
+                reader.ReadBytes(byteArray);
+                Base64String = Convert.ToBase64String(byteArray);
+            }
+
+            return Base64String;
+        }
+    }
+
+}
